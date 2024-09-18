5688c6cc954fcd298523a7bf595f65b95d27d735
islam <islamdomka2005@gmail.com>
2024-09-18 20:15:07 +0300

    widget_file_update

--- src/widget.py
+++ src/widget.py
@@ -1,24 +1,21 @@
 from typing import Union
 
+from masks import get_mask_account, get_mask_card_number
 
-def mask_account_card(account_number_card: Union[str]) -> Union[str]:
+
+def mask_account_card(account_number_card: str) -> str:
     """Функция принимает на вход номер карты или счета
     и возвращает зашифрованный номер карты или счета с указанием"""
     for element in account_number_card.split():
         try:
             int(element)
             if len(element) == 16:
-                separation_card_number = [element[i : i + 4] for i in range(0, len(element), 4)]
-                separation_card_number[2] = "****"
-                separation_card_number[1] = separation_card_number[1][:2] + "**"
-                return (
-                    f"{" ".join(account_number_card.split()[:account_number_card.split().index(element)])} "
-                    f"{" ".join(separation_card_number)}"
-                )
+                return f"{" ".join(account_number_card.split()[:-1])} {get_mask_card_number(element)}"
             elif len(element) == 20:
-                return account_number_card.split()[0] + " **" + element[-4:]
+                return f"{" ".join(account_number_card.split()[:-1])} {get_mask_account(element)}"
         except ValueError:
-            return "-1"
+            continue
+    return "-1"
 
 
 def get_date(date_format: Union[str]) -> str:
