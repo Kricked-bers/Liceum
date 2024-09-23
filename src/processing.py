bc4cf4a0d55f5e2374776e141d4131e11157438a
islam <islamdomka2005@gmail.com>
2024-09-23 19:42:54 +0300

    change get_date on widget.py and make filter_by_state and sort_by_date in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,26 @@
-def filter_by_state(dictionary: list, state_input: str = "EXECUTED") -> [list, str]:
+from datetime import datetime
+from typing import Union
+
+from src.widget import get_date
+
+
+def filter_by_state(dictionary: list, state_input: str = "EXECUTED") -> Union[list, str]:
     exit_lists = list()
     for element_lists in dictionary:
-        if element_lists['state'] == state_input:
+        if element_lists["state"] == state_input:
             exit_lists.append(element_lists)
     return exit_lists
 
 
-def sort_by_date():
-    pass
-
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                 {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                 {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                 {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], "CANCELED"))
+def sort_by_date(dictionary: list) -> list:
+    dates_nosort = [get_date(element["date"]) for element in dictionary]
+    dates_format_datetime = [datetime.strptime(date, "%Y-%m-%d") for date in dates_nosort]
+    dates_format_datetime.sort(reverse=True)
+    dates_format_normal = [runcode_date.strftime("%Y-%m-%d") for runcode_date in dates_format_datetime]
+    dictionary_dates_sort = list()
+    for normal_date in dates_format_normal:
+        for dictionary_element in dictionary:
+            element_get_date = get_date(dictionary_element["date"])
+            if element_get_date == normal_date:
+                dictionary_dates_sort.append(dictionary_element)
+    return dictionary_dates_sort
