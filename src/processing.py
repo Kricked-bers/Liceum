0eb4ac118616ddc25a8930ca6adae6e073388f1f
islam <islamdomka2005@gmail.com>
2024-09-24 11:52:45 +0300

    end_path

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,9 @@
 from datetime import datetime
-from typing import Union
 
 from src.widget import get_date
 
 
-def filter_by_state(dictionary: list, state_input: str = "EXECUTED") -> Union[list, str]:
+def filter_by_state(dictionary: list, state_input: str = "EXECUTED") -> list:
     """Функция принимает на вход список словарей и условие
     и возвращает словари подходящие под условие"""
     exit_lists = list()
@@ -19,7 +18,7 @@ def sort_by_date(dictionary: list, filter_input: str = "True") -> list:
     и возвращает список словарей отсортированный по условию по датам"""
     dates_nosort = [get_date(element["date"]) for element in dictionary]
     dates_format_datetime = [datetime.strptime(date, "%Y-%m-%d") for date in dates_nosort]
-    dates_format_datetime.sort(reverse=filter_input)
+    dates_format_datetime.sort(reverse=bool(filter_input))
     dates_format_normal = [runcode_date.strftime("%Y-%m-%d") for runcode_date in dates_format_datetime]
     dictionary_dates_sort = list()
     for normal_date in dates_format_normal:
