fc7b55cb85ca31121a5b4f5d768cf6659e22e34a
islam <islamdomka2005@gmail.com>
2024-10-07 20:56:05 +0300

    change test_masks.py, test_processing.py. Update fixture and parametrize

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,10 +1,14 @@
+import pytest
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_get_mask_account_basic():
-    assert get_mask_account("12548569562145875236") == "**5236"
-    assert get_mask_account("65895452658452456985") == "**6985"
-    assert get_mask_account("56895625458565856556") == "**6556"
+@pytest.mark.parametrize(
+    "input, exit",
+    [("12548569562145875236", "**5236"), ("65895452658452456985", "**6985"), ("56895625458565856556", "**6556")],
+)
+def test_get_mask_account_basic(input, exit):
+    assert get_mask_account(input) == exit
 
 
 def test_get_mask_account_empty():
@@ -16,10 +20,16 @@ def test_get_mask_account_type():
     assert get_mask_account([1, 3, 5]) == "-1"
 
 
-def test_get_mask_card_number_basic():
-    assert get_mask_card_number("7854695815624587") == "7854 69** **** 4587"
-    assert get_mask_card_number("1254869562154658") == "1254 86** **** 4658"
-    assert get_mask_card_number("4587698514523654") == "4587 69** **** 3654"
+@pytest.mark.parametrize(
+    "input_1, exit_1",
+    [
+        ("7854695815624587", "7854 69** **** 4587"),
+        ("1254869562154658", "1254 86** **** 4658"),
+        ("4587698514523654", "4587 69** **** 3654"),
+    ],
+)
+def test_get_mask_card_number_basic(input_1, exit_1):
+    assert get_mask_card_number(input_1) == exit_1
 
 
 def test_get_mask_card_number_empty():
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,11 +1,18 @@
+import pytest
+
 from src.processing import filter_by_state, sort_by_date
 
-input_dict = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
+
+@pytest.fixture
+def input_dict():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
 exit_dict_filter_by_state_executed = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -28,7 +35,7 @@ exit_dict_sort_by_date_false = [
 ]
 
 
-def test_filter_by_state_basic_state():
+def test_filter_by_state_basic_state(input_dict):
     assert filter_by_state(input_dict, "EXECUTED") == exit_dict_filter_by_state_executed
     assert filter_by_state(input_dict, "CANCELED") == exit_dict_filter_by_state_canceled
 
@@ -43,12 +50,12 @@ def test_filter_by_state_type():
     assert filter_by_state(52445) == "-1"
 
 
-def test_filter_by_state_type_state():
+def test_filter_by_state_type_state(input_dict):
     assert filter_by_state(input_dict, 52154) == "-1"
     assert filter_by_state(input_dict, ("NotStr", "15")) == "-1"
 
 
-def test_sort_by_date_basic_default():
+def test_sort_by_date_basic_default(input_dict):
     assert sort_by_date(input_dict) == exit_dict_sort_by_date_true
 
 
@@ -56,12 +63,12 @@ def test_sort_by_date_empty():
     assert sort_by_date([]) == "-1"
 
 
-def test_sort_by_date_type():
+def test_sort_by_date_type(input_dict):
     assert sort_by_date(4521) == "-1"
     assert sort_by_date(input_dict, False) == "-1"
     assert sort_by_date({1: 2, 2: 2}, "False") == "-1"
 
 
-def test_sort_by_date_filter():
+def test_sort_by_date_filter(input_dict):
     assert sort_by_date(input_dict, "True") == exit_dict_sort_by_date_true
     assert sort_by_date(input_dict, "False") == exit_dict_sort_by_date_false
