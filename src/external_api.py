3588183f99f2f2f7e6ecd7003069b0ac2bc0f581
islam <islamdomka2005@gmail.com>
2024-10-22 15:46:42 +0300

    small changes on external_api.py, new file .env.example and remake readme.md

--- src/external_api.py
+++ src/external_api.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 
 
 def transaction_analiz(transaction: dict) -> float:
-    """Функуия принимает на вход транзакцию в виде словаря и возвращает сумму транзакции в рублях,
-    если транзакция была произведена в другой валюте функция обращается к внешнему API и производит конвертацию"""
+    '''Функуия принимает на вход транзакцию в виде словаря и возвращает сумму транзакции в рублях,
+    если транзакция была произведена в другой валюте функция обращается к внешнему API и производит конвертацию'''
     load_dotenv()
     api_token = os.getenv("API_KEY")
     headers = {"apikey": api_token}
@@ -21,15 +21,5 @@ def transaction_analiz(transaction: dict) -> float:
         }
         url = "https://api.apilayer.com/exchangerates_data/convert"
         response = requests.get(url, params=params, headers=headers)
-        print(type(response.text))
         amount_from_rub = json.loads(response.text)
         return float(amount_from_rub["result"])
-
-
-ans_dict = {
-    "success": True,
-    "query": {"from": "USD", "to": "RUB", "amount": 8221.37},
-    "info": {"timestamp": 1729587482, "rate": 96.82903},
-    "date": "2024-10-22",
-    "result": 555.555,
-}
